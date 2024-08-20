7982ed6f765bfc4867a8bf081b66b10186966c28
Enrico Bessa <86358593+EnricoBessa@users.noreply.github.com>
2024-08-20 11:08:34 -0300

    Update Ex5_6_7.java

--- ExJavaBasico/Ex5_6_7.java
+++ ExJavaBasico/Ex5_6_7.java
@@ -8,13 +8,18 @@ public class Ex5_6_7 {
         String outputFile = "C:\\Users\\enric\\Documents\\programação\\java\\LPM\\ExJavaBasico\\criptografado.txt";
 
         FileOutputStream outputStream = new FileOutputStream(outputFile);
-
-        criptografar(inputStream, outputStream);
+        
+        criptografar(new FileInputStream(inputFile), outputStream);
 
         System.out.println("Arquivo criptografado com sucesso!");
-
     }
 
+    /**
+     * Criptografa o conteúdo do arquivo de entrada e escreve no arquivo de saída.
+     * @param inputStream O fluxo de entrada do arquivo que será criptografado.
+     * @param outputStream O fluxo de saída para o arquivo criptografado.
+     * @throws IOException Se ocorrer um erro de entrada/saída durante a leitura ou escrita dos arquivos.
+     */
     public static void criptografar(FileInputStream inputStream, FileOutputStream outputStream) throws IOException {
         int byteData = inputStream.read();
 
