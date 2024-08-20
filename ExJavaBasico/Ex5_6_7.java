879d1603749fe0acfc68db920cccfb4e9379cfaa
Enrico Bessa <86358593+EnricoBessa@users.noreply.github.com>
2024-08-20 10:54:06 -0300

    Update Ex5_6_7.java

--- ExJavaBasico/Ex5_6_7.java
+++ ExJavaBasico/Ex5_6_7.java
@@ -1,5 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Ex5_6_7 {
     public static void main(String[] args) {
-        
+        String inputFile = "C:\\Users\\enric\\Documents\\programação\\java\\LPM\\ExJavaBasico\\original.txt";
+        String outputFile = "C:\\Users\\enric\\Documents\\programação\\java\\LPM\\ExJavaBasico\\criptografado.txt";
+
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+        criptografar(inputStream, outputStream);
+
+        System.out.println("Arquivo criptografado com sucesso!");
+
+    }
+
+    public static void criptografar(FileInputStream inputStream, FileOutputStream outputStream) throws IOException {
+        int byteData = inputStream.read();
+
+        if (byteData != -1) {
+            outputStream.write(byteData + 1);
+            criptografar(inputStream, outputStream);
+        }
     }
 }
