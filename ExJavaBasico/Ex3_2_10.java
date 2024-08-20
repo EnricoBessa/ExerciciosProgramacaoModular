af97e8d53ba05ff69374da00874d0d5a002034f8
Enrico Bessa <86358593+EnricoBessa@users.noreply.github.com>
2024-08-20 11:01:11 -0300

    Update Ex3_2_10.java

--- ExJavaBasico/Ex3_2_10.java
+++ ExJavaBasico/Ex3_2_10.java
@@ -24,12 +24,21 @@ public class Ex3_2_10{
         //scanner.close();
         return valor;
     }
-
+    /**
+     * Recebe o salario e a quantidade de horas extra. Calcula de acordo com essas informações quanto deve rebecer em dinheiro. Retorna um double. 
+     * @param salario O salário mensal do funcionário.
+     * @param hr_extra A quantidade de horas extras realizadas.
+     * @return Double que contém quantas horas extra foram feitas.
+     */
     public static double calcular_hr_extra(double salario, double hr_extra){
         double hr_extra_resultado = (( salario / 176) * hr_extra) * salario/2;
         return hr_extra_resultado;
     }
-
+    /**
+     * Recebe o salario e calcula de acordo com esse valor o quanto deve pagar de INSS limitando a 150 reais. Retorna um double. 
+     * @param salario O salário mensal do funcionário.
+     * @return Double que contém o valor a pagar de INSS.
+     */
     public static double calcular_inss(double salario){
         double inss_resultado = salario * 0.1;
         if(inss_resultado > 150)
